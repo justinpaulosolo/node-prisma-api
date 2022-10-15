fc7b90482af629ec669794270a6f717df603e171
justinpaulosolo <justinpaulosolo@gmail.com>
2022-10-14 19:07:52 -0700

    Hapi server decoupled

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,5 @@
-import Hapi from "@hapi/hapi";
+import { createServer, startServer } from "./utils/server";
 
-const server = Hapi.server({
-  port: process.env.PORT || 3000,
-  host: process.env.HOST || "localhost",
-});
-
-export async function start(): Promise<Hapi.Server> {
-  await server.start();
-  return server;
-}
-
-process.on("unhandledRejection", (err) => {
-  console.log(err);
-  process.exit(1);
-});
-
-start()
-  .then((server) => {
-    console.log(`Server running at: ${server.info.uri}`);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+createServer()
+  .then(startServer)
+  .catch((err) => console.log(err));
